91d8a6b6d2d0a141964bf54f67de70b5cc715099
Zhi-Qiang You <zyou@osc.edu>
2021-06-15 23:53:49 -0400

    Update get keys script

--- ingest.sh
+++ ingest.sh
@@ -3,10 +3,7 @@
 logfile=$1
 
 module use $HOME/lmodfiles/Core
-#module load xalt/2.10.10-syslog
-module load xalt/2.10.11-syslog
-#module load xalt/2.10.14-syslog
-#module load xalt/osc
+module load xalt/2.10.18-syslog
 module list
 
 if [ -z "${logfile}" ] || [ ! -f ${logfile} ]; then
@@ -17,12 +14,12 @@ fi
 env |grep XALT
 
 export XALT_EXECUTABLE_TRACKING="yes"
-#export XALT_DB_CONF=/fs/ess/PZS0710/xalt/etc/pitzer/xalt_db.conf
 export XALT_DB_CONF=$XALT_ETC_DIR/xalt_db.conf
 #source $HOME/software_usage/venv/bin/activate
+python --version
 set -x
-rm -f leftover*.log
-python2 $OSC_XALT_DIR/sbin/xalt_syslog_to_db.py \
+rm -f leftover*.log*
+python $OSC_XALT_DIR/sbin/xalt_syslog_to_db.py \
 	--syshost pitzer \
 	--reverseMapD $XALT_ETC_DIR/reverseMapD \
  	--leftover leftover_pitzer.log \
